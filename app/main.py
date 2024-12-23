16d22346f353006019c348813df767fde896cace
Szeri <szeri323@gmail.com>
2024-12-23 21:50:27 +0100

    [add cat-file]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import sys
 import os
-
+import zlib
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
     command = sys.argv[1]
+    
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
@@ -14,6 +15,15 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    if command == "cat-file":
+        flag = sys.argv[2]
+        hash_name = sys.argv[3]
+        if flag == "-p":
+            with open(f".git/objects/{hash_name[:2]}/{hash_name[2:]}", "rb") as f:
+                blob = f.read()
+                decompressed_content = zlib.decompress(blob)
+                length = decompressed_content[6:]
+                print(decompressed_content[decompressed_content.find(b'\x00')+1:].decode('utf8'), end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
